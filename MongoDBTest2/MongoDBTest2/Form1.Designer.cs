6514f53bf4dcc5850e33e9c1a3c0c3a017120a7f
Tuyen <tuyenqd@gcs-vn.com>
2018-07-24 09:29:40 +0700

    add new link

---
+++ MongoDBTest2/MongoDBTest2/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MongoDBTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.rbEntity1 = new System.Windows.Forms.RadioButton();
+            this.rbEntity2 = new System.Windows.Forms.RadioButton();
+            this.rbEntity3 = new System.Windows.Forms.RadioButton();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnFindAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(38, 228);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name : ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(147, 101);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Location";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(147, 137);
+            this.txtAge.MaxLength = 2;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 5;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(147, 167);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtLocation.TabIndex = 6;
+            // 
+            // rbEntity1
+            // 
+            this.rbEntity1.AutoSize = true;
+            this.rbEntity1.Checked = true;
+            this.rbEntity1.Location = new System.Drawing.Point(57, 42);
+            this.rbEntity1.Name = "rbEntity1";
+            this.rbEntity1.Size = new System.Drawing.Size(57, 17);
+            this.rbEntity1.TabIndex = 7;
+            this.rbEntity1.TabStop = true;
+            this.rbEntity1.Text = "Entity1";
+            this.rbEntity1.UseVisualStyleBackColor = true;
+            this.rbEntity1.CheckedChanged += new System.EventHandler(this.rbEntity1_CheckedChanged);
+            // 
+            // rbEntity2
+            // 
+            this.rbEntity2.AutoSize = true;
+            this.rbEntity2.Location = new System.Drawing.Point(147, 42);
+            this.rbEntity2.Name = "rbEntity2";
+            this.rbEntity2.Size = new System.Drawing.Size(57, 17);
+            this.rbEntity2.TabIndex = 7;
+            this.rbEntity2.Text = "Entity2";
+            this.rbEntity2.UseVisualStyleBackColor = true;
+            this.rbEntity2.CheckedChanged += new System.EventHandler(this.rbEntity2_CheckedChanged);
+            // 
+            // rbEntity3
+            // 
+            this.rbEntity3.AutoSize = true;
+            this.rbEntity3.Location = new System.Drawing.Point(245, 42);
+            this.rbEntity3.Name = "rbEntity3";
+            this.rbEntity3.Size = new System.Drawing.Size(57, 17);
+            this.rbEntity3.TabIndex = 7;
+            this.rbEntity3.Text = "Entity3";
+            this.rbEntity3.UseVisualStyleBackColor = true;
+            this.rbEntity3.CheckedChanged += new System.EventHandler(this.rbEntity3_CheckedChanged);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(147, 228);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(100, 23);
+            this.btnFind.TabIndex = 8;
+            this.btnFind.Text = "Find By Name";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnFindAll
+            // 
+            this.btnFindAll.Location = new System.Drawing.Point(38, 268);
+            this.btnFindAll.Name = "btnFindAll";
+            this.btnFindAll.Size = new System.Drawing.Size(75, 23);
+            this.btnFindAll.TabIndex = 9;
+            this.btnFindAll.Text = "Find All";
+            this.btnFindAll.UseVisualStyleBackColor = true;
+            this.btnFindAll.Click += new System.EventHandler(this.btnFindAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 329);
+            this.Controls.Add(this.btnFindAll);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.rbEntity3);
+            this.Controls.Add(this.rbEntity2);
+            this.Controls.Add(this.rbEntity1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Mongo Database Test with C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.RadioButton rbEntity1;
+        private System.Windows.Forms.RadioButton rbEntity2;
+        private System.Windows.Forms.RadioButton rbEntity3;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnFindAll;
+        
+    }
+}
+
---
+++ MongoDBTest2/MongoDBTest2/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+using CommonTools.TreeList;
+
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+
+namespace MongoDBTest2
+{
+    public partial class Form1 : Form
+    {
+        MongoServer _server;
+        MongoDatabase _database;
+        MongoCollection _collection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connection = "mongodb://localhost:27017";
+            //string connection = ConfigurationSettings.AppSettings["Connection"];
+
+            _server = MongoServer.Create(connection);
+            _database = _server.GetDatabase(ConfigurationSettings.AppSettings["Database"], SafeMode.True);
+        }
+
+        public class Users1
+        {
+            public ObjectId Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+        }
+
+        public class Users2
+        {
+            public ObjectId Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Location { get; set; }
+        }
+
+        public class Users3
+        {
+            public ObjectId Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Location { get; set; }
+        }
+
+        private void rbEntity1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtName.Enabled = true;
+            txtAge.Enabled = true;
+            txtLocation.Enabled = true;
+        }
+
+        private void rbEntity2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtName.Enabled = true;
+            txtAge.Enabled = false;
+            txtLocation.Enabled = true;
+        }
+
+        private void rbEntity3_CheckedChanged(object sender, EventArgs e)
+        {
+            txtName.Enabled = true;
+            txtAge.Enabled = true;
+            txtLocation.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (rbEntity1.Checked)
+            {
+                var _users = _database.GetCollection<Users3>("users");
+                var user = new Users3 { };
+                user.Age = Convert.ToInt32(txtAge.Text);
+                user.Name = txtName.Text;
+                user.Location = txtLocation.Text;
+                _users.Insert(user);
+                var id = user.Id;
+            }
+            else if (rbEntity2.Checked)
+            {
+                var _users = _database.GetCollection<Users2>("users");
+                var user = new Users2 { };
+                user.Name = txtName.Text;
+                user.Location = txtLocation.Text;
+                _users.Insert(user);
+                var id = user.Id;
+            }
+            else if (rbEntity3.Checked)
+            {
+                var _users = _database.GetCollection<Users1>("users");
+                var user = new Users1 { };
+                user.Age = Convert.ToInt32(txtAge.Text);
+                user.Name = txtName.Text;
+                _users.Insert(user);
+                var id = user.Id;
+                
+            }
+            MessageBox.Show("User with name " + txtName.Text + " created");
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            _collection = _database.GetCollection<Users3>("users");
+            IMongoQuery query = Query.EQ("Name", txtName.Text);
+            Users3 _user = _collection.FindAs<Users3>(query).FirstOrDefault();
+            if (_user != null)
+            {
+                MessageBox.Show(_user.Age.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Not able to find any results with name " + txtName.Text);
+            }
+        }
+
+        private void btnFindAll_Click(object sender, EventArgs e)
+        {
+            _collection = _database.GetCollection<Users3>("users");
+            var userCursor = _collection.FindAllAs<Users3>();
+            int count = 0;
+            foreach (var item in userCursor)
+            {
+               
+                //Update some values
+                item.Age = 30;
+                _collection.Save(item);
+
+                //_collection.Remove(
+
+                //TreeNode tNode = new TreeNode("(" + count.ToString() + ") {..}");
+                //tvUsers_1.Nodes.Add(tNode);
+
+                
+                //CommonTools.Node tListNode1 = new CommonTools.Node("(" + count.ToString() + ") {..}");
+
+                //CommonTools.Node tListNode2 = new CommonTools.Node("Test");
+                //object[] array1 = new object[2];
+                //array1[0]="fieldname0";
+                //array1[1]="fieldname1";
+                //tListNode2.SetData(array1);
+                //tListNode1.Nodes.Add(tListNode2);
+                //treeListView1.Nodes.Add(tListNode1);
+
+
+                //MessageBox.Show(item.ToJson());
+                count++;
+
+                //TreeNode node2 = new TreeNode("C#");
+                //TreeNode node3 = new TreeNode("VB.NET");
+                //TreeNode[] array = new TreeNode[] { node2, node3 };
+            }
+        }
+    }
+}
