1b9401ed4f28ff1784679dacb15855bb046fa78a
TuyenDao <daotuyen9244@gmail.com>
2018-07-28 22:33:18 +0700

    add new link

---
+++ W25Q16/W25Q16/MDK-ARM/STM32F103C8_Temp/bsp_spi_flash.d
@@ -0,0 +1,12 @@
+stm32f103c8_temp\bsp_spi_flash.o: ..\Src\bsp_spi_flash.c
+stm32f103c8_temp\bsp_spi_flash.o: ..\Inc\bsp_spi_flash.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+stm32f103c8_temp\bsp_spi_flash.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Inc\stm32f10x_conf.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_spi.h
+stm32f103c8_temp\bsp_spi_flash.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_usart.h
---
+++ W25Q16/W25Q16/MDK-ARM/STM32F103C8_Temp/main.d
@@ -0,0 +1,14 @@
+stm32f103c8_temp\main.o: ../Src/main.c
+stm32f103c8_temp\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+stm32f103c8_temp\main.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+stm32f103c8_temp\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f103c8_temp\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+stm32f103c8_temp\main.o: ..\Inc\stm32f10x_conf.h
+stm32f103c8_temp\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+stm32f103c8_temp\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+stm32f103c8_temp\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+stm32f103c8_temp\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_spi.h
+stm32f103c8_temp\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_usart.h
+stm32f103c8_temp\main.o: ..\Inc\bsp_usart1.h
+stm32f103c8_temp\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+stm32f103c8_temp\main.o: ..\Inc\bsp_spi_flash.h
